995ad2bbbd96f5cff784cd1ee3f8827a9d3b0713
Anastasiia <anastasia.yershova.00@gmail.com>
2024-03-29 23:41:34 +0000

    fix import

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,18 @@
-import { Box } from "@chakra-ui/react";
-
 import Resume from "../../components/Resume";
 import { ContactForm } from "../../components/ContactForm";
 import { colors } from "../../styles";
 import { containerStyles } from "../../styles/projectStyles";
+import { Box } from "@mui/material";
 
 const About = () => {
   return (
     <>
-      <Box bgColor={colors.primary.main}>
+      <Box bgcolor={colors.primary.main}>
         <Box p="20px" sx={containerStyles}>
           <Resume />
         </Box>
       </Box>
-      <Box id="contact" bgColor={colors.secondary.main} sx={containerStyles}>
+      <Box id="contact" bgcolor={colors.secondary.main} sx={containerStyles}>
         <ContactForm type="block" />
       </Box>
     </>
